4df7f7c73a119038daf37160303508bef380796a
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-02 23:53:40 +0200

    format enemy.hpp

--- enemy/enemy.hpp
+++ enemy/enemy.hpp
@@ -1,45 +1,31 @@
-#include <vector>
 #include <SFML/Graphics>
+#include <vector>
 
 #ifndef ENEMY_HPP
 #define ENEMY_HPP
 
-namespace fnad
-{
-    enum class Status
-    {
-        susceptible,
-        infectious,
-        removed
-    };
+namespace fnad {
+enum class Status { susceptible, infectious, removed };
 
-    enum class Floor
-    {
-        underground,
-        ground,
-        first,
-        second,
-        roof
-    };
+enum class Floor { underground, ground, first, second, roof };
 
-    struct Position
-    {
-        sf::Vector2f coordinates;
-        Floor floor;
-    };
+struct Position {
+  sf::Vector2f coordinates;
+  Floor floor;
+};
 
-    class Enemy
-    {
-    private:
-        Status status_;
-        Position position_;
+class Enemy {
+ private:
+  Status status_;
+  Position position_;
 
-    public:
-        Enemy(Status status, Position position) : status_{status}, position_{position} {}
-        Enemy(Status status) : Enemy(status, sf::Vector2f{0.f, 0.f}) {}
-        Enemy() : Enemy(Status::susceptible) {}
-        void evolve();
-    };
-}
+ public:
+  Enemy(Status status, Position position)
+      : status_{status}, position_{position} {}
+  Enemy(Status status) : Enemy(status, sf::Vector2f{0.f, 0.f}) {}
+  Enemy() : Enemy(Status::susceptible) {}
+  void evolve();
+};
+}  // namespace fnad
 
 #endif
