02a684256be8c87a490f74047347364a5dff0bb8
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-02 23:07:47 +0200

    modify Enemy class && include SFML Graphics

--- enemy/enemy.hpp
+++ enemy/enemy.hpp
@@ -1,18 +1,42 @@
+#ifndef ENEMY_HPP
+#define ENEMY_HPP
+
 #include <vector>
+#include <SFML/Graphics>
 
 namespace fnad
 {
-    enum class Status {susceptible, infectious, removed};
+    enum class Status
+    {
+        susceptible,
+        infectious,
+        removed
+    };
+
+    enum class Floor
+    {
+        underground,
+        ground,
+        first,
+        second,
+        roof
+    };
+
+    struct Position
+    {
+        sf::Vector2f position;
+        int floor;
+    };
 
     class Enemy
     {
     private:
-    Status status{Status::susceptible};
-    std::vector<float> position;
-    int floor;
+        Status status_{Status::susceptible};
+        Position position_;
 
     public:
-    void move(float, float);
-    void evolve();
+        void evolve();
     };
 }
+
+#endif
