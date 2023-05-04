a3fe5a171a10acf1f130cebb46dfd1f5090847c4
Luca Zoppetti <luckeedev@gmail.com>
2023-05-04 23:02:40 +0200

    add infect method and test enemy class

--- enemy/enemy.hpp
+++ enemy/enemy.hpp
@@ -18,13 +18,25 @@ class Enemy {
  private:
   Status status_;
   Position position_;
+  // Defined in pixels/second
+  float speed_;
 
  public:
   Enemy(Status status, Position position)
       : status_{status}, position_{position} {}
-  Enemy(Status status) : Enemy(status, Position{sf::Vector2f{0.f, 0.f}, Floor::underground}) {}
+  Enemy(Status status)
+      : Enemy(status, Position{sf::Vector2f{0.f, 0.f}, Floor::underground}) {}
   Enemy() : Enemy(Status::susceptible) {}
-  void evolve();
+
+  const Status status() const;
+  const Position position() const;
+  const float speed() const;
+
+  // Accept a delta time object indicating how much time has passed since the
+  // last render
+  void evolve(sf::Time);
+
+  void infect();
 };
 }  // namespace fnad
 
--- enemy/enemy.test.cpp
+++ enemy/enemy.test.cpp
@@ -1,1 +1,39 @@
+#include "enemy.hpp"
+
+#include <cmath>
+
 #include "../test/doctest.h"
+
+TEST_CASE("Testing the Enemy class") {
+  fnad::Enemy enemy;
+
+  SUBCASE("Calling evolve moves the enemy") {
+    auto position_before = enemy.position();
+
+    sf::Time time{sf::seconds(1.f)};
+
+    enemy.evolve(time);
+
+    auto position_after = enemy.position();
+
+    CHECK_EQ(position_before.floor, position_after.floor);
+
+    auto distance = std::sqrt(
+        std::pow((position_after.coordinates.x - position_before.coordinates.x),
+                 2) +
+        std::pow((position_after.coordinates.y - position_before.coordinates.y),
+                 2));
+
+    CHECK_EQ(distance, doctest::Approx(time.asSeconds() * enemy.speed()));
+  }
+
+  SUBCASE("Calling infect changes the status") {
+    auto status_before = enemy.status();
+
+    enemy.infect();
+
+    auto status_after = enemy.status();
+
+    CHECK_EQ(status_after, fnad::Status::infectious);
+  }
+}
