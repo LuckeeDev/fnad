a99852274ccef7cf860b96d1fbea277b546b55c8
Iolanda Piccio <iolanda.piccio@gmail.com>
2023-05-05 17:55:34 +0200

    create tests character

--- character/character.test.cpp
+++ character/character.test.cpp
@@ -1,1 +1,18 @@
+#include "character.hpp"
+
+#include "../enemy/enemy.hpp"
 #include "../test/doctest.h"
+
+TEST_CASE("Testing the Character class") {
+  Character character;
+
+  SUBCASE("Detect contact with infectious enemy") {
+    Enemy infectious(Status::infectious);
+    CHECK_EQ(character.checkContact(infectious&), true);
+  }
+
+  SUBCASE("Detect contact with non infectious enemy") {
+    Enemy susceptible(Status::susceptible);
+    CHECK_EQ(character.checkContact(susceptible&), false);
+  }
+}
