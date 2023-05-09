3ad7723050c3217f64ea85018821d31176b4141f
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-09 23:54:03 +0200

    declare Epidemic::getEnemies and Epidemic::count && initialize parameters

--- src/epidemic/epidemic.hpp
+++ src/epidemic/epidemic.hpp
@@ -1,28 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include "../entities/enemy/enemy.hpp"
 
 #ifndef EPIDEMIC_HPP
 #define EPIDEMIC_HPP
 
 namespace fnad {
-class Enemy;
+//class Enemy;
 
 class Epidemic {
  private:
   std::vector<Enemy> enemies_;
 
   double s_;
-  double i_;
-  double r_;
+  double i_{1.};
+  double r_{};
 
-  double beta_;
-  double gamma_;
+  double beta_{0.7};
+  double gamma_{0.4};
 
   // Rate of how many days pass each second
-  double days_per_second_;
+  double days_per_second_{1};
 
  public:
   Epidemic(int, sf::Vector2f);
+
+  std::vector<Enemy> const& getEnemies() const;
+  int count(Status const&) const;
 
   void evolve(const sf::Time&);
 };
