d268b6cacb4c9d9c206b836fc1ebd067ce9b1762
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-09 23:39:14 +0200

    write tests for Epidemic::evolve && change CMakeLists.txt

---
+++ src/epidemic/epidemic.test.cpp
@@ -0,0 +1,27 @@
+#include "epidemic.hpp"
+
+#include "../../test/doctest.h"
+
+TEST_CASE("Testing Epidemic") {
+  SUBCASE("Testing evolve") {
+    fnad::Epidemic epidemic(100, sf::Vector2f{960.f, 540.f});
+
+    int s_0{epidemic.count(fnad::Status::susceptible)},
+        i_0{epidemic.count(fnad::Status::infectious)},
+        r_0{epidemic.count(fnad::Status::removed)};
+
+    epidemic.evolve(sf::seconds(500.f));
+
+    int s_1{epidemic.count(fnad::Status::susceptible)},
+        i_1{epidemic.count(fnad::Status::infectious)},
+        r_1{epidemic.count(fnad::Status::removed)};
+
+    CHECK(s_0 == 99);
+    CHECK(i_0 == 1);
+    CHECK(r_0 == 0);
+    CHECK(s_0 + i_0 + r_0 == s_1 + i_1 + r_1);
+    CHECK(s_0 != s_1);
+    CHECK(i_0 != i_1);
+    CHECK(r_0 != r_1);
+  }
+}
