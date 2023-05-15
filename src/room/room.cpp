be98a2abe0874814ad1d447066b7966a700d4d1c
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-16 00:01:53 +0200

    define Room's constructor

--- src/room/room.cpp
+++ src/room/room.cpp
@@ -1,1 +1,7 @@
 #include "room.hpp"
+
+namespace fnad {
+Room::Room(sf::Vector2f const& position, sf::Vector2f const& size,
+           Floor const& floor)
+    : position_{position}, size_{size}, floor_{floor} {}
+}  // namespace fnad
