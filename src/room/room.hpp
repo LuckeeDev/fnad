9f1236c0e3927ca4f4da0cf62da9342eadb17035
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-16 00:00:59 +0200

    declare Room members

--- src/room/room.hpp
+++ src/room/room.hpp
@@ -2,7 +2,17 @@
 #define ROOM_HPP
 #include <SFML/Graphics.hpp>
 
+#include "../common/floor.hpp"
+
 namespace fnad {
-class Room : public sf::IntRect {};
+class Room : public sf::FloatRect {
+ private:
+  sf::Vector2f position_;
+  sf::Vector2f size_;
+  Floor floor_;
+
+ public:
+  Room(sf::Vector2f const&, sf::Vector2f const&, Floor const&);
+};
 }  // namespace fnad
 #endif
