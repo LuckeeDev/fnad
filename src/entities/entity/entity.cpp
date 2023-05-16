fc28e35689216ec474cd6cd1f2d357c3d81f12ab
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-16 11:38:59 +0200

    change Entity's interface
    add map_ptr_ && room_ptr_
    fix constructor
    fix setFloor
    add getMap && getRoom (declare and define)

--- src/entities/entity/entity.cpp
+++ src/entities/entity/entity.cpp
@@ -1,16 +1,51 @@
 #include "entity.hpp"
 
 namespace fnad {
-Entity::Entity(Floor floor, sf::Vector2f position, float speed)
+Entity::Entity(Map& map, sf::Vector2f position, float speed)
     : sf::RectangleShape(sf::Vector2f{10.f, 10.f}),
-      floor_{floor},
+      map_ptr_{&map},
+      room_ptr_{nullptr},
+      floor_{map.getFloor()},
       speed_{speed} {
   setPosition(position);
+
+  auto rooms = map.getRooms();
+  for (auto& room : rooms) {
+    if (room.contains(position)) {
+      room_ptr_ = &room;
+      break;
+    }
+  }
+
+  if (room_ptr_ == nullptr) {
+    throw std::invalid_argument("Cannot create an entity outside the map.");
+  }
 };
 
-void Entity::setFloor(const Floor& floor) { floor_ = floor; }
+void Entity::setFloor(Map& map, sf::Vector2f position) {
+  floor_ = map.getFloor();
+
+  setPosition(position);
+
+  auto rooms = map.getRooms();
+  for (auto& room : rooms) {
+    if (room.contains(position)) {
+      room_ptr_ = &room;
+      break;
+    }
+  }
+
+  if (room_ptr_ == nullptr) {
+    throw std::invalid_argument("Cannot put an entity outside the map.");
+  }
+}
 void Entity::setSpeed(const float& speed) { speed_ = speed; }
 
+Map const& Entity::getMap() const { return *map_ptr_; }
+
+Room const& Entity::getRoom() const { return *room_ptr_; }
+
 Floor Entity::getFloor() const { return floor_; }
+
 float Entity::getSpeed() const { return speed_; }
 }  // namespace fnad
--- src/entities/entity/entity.hpp
+++ src/entities/entity/entity.hpp
@@ -4,20 +4,26 @@
 #include <SFML/Graphics.hpp>
 
 #include "../../common/floor.hpp"
+#include "../../map/map.hpp"
+#include "../../room/room.hpp"
 
 namespace fnad {
 class Entity : public sf::RectangleShape {
  protected:
+  Map* map_ptr_;
+  Room* room_ptr_;
   Floor floor_;
   // Defined in pixels/second
   float speed_;
 
-  Entity(Floor, sf::Vector2f, float);
+  Entity(Map&, sf::Vector2f, float);
 
  public:
-  void setFloor(const Floor&);
+  void setFloor(Map&, sf::Vector2f);
   void setSpeed(const float&);
 
+  Map const& getMap() const;
+  Room const& getRoom() const;
   Floor getFloor() const;
   float getSpeed() const;
 };
