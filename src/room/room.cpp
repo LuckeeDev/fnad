765e0187ce517d646f12864db9eea821cb00ed27
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-16 09:53:04 +0200

    fix Room's constructor

--- src/room/room.cpp
+++ src/room/room.cpp
@@ -3,5 +3,5 @@
 namespace fnad {
 Room::Room(sf::Vector2f const& position, sf::Vector2f const& size,
            Floor const& floor)
-    : position_{position}, size_{size}, floor_{floor} {}
+    : sf::FloatRect(position, size), floor_{floor} {}
 }  // namespace fnad
