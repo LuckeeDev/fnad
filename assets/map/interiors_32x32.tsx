5fdc7e0f3aca20c31efcd70b82c15f7d1042058b
Luca Zoppetti <luckeedev@gmail.com>
2023-05-17 09:13:13 +0200

    reorder map files to work in all cases

--- assets/map/interiors_32x32.tsx
+++ assets/map/interiors_32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="Modern Interiors Complete" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
- <image source="../Tilesets/Modern Interiors/Interiors_free/32x32/Interiors_free_32x32.png" width="512" height="2848"/>
+ <image source="./tilesets/interiors_32x32.png" width="512" height="2848"/>
 </tileset>
