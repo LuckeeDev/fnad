521ec3f369b35bfce1afa7507e60dc3cf5168dc0
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-19 21:55:34 +0200

    change spawning range

--- src/spawner/spawner.cpp
+++ src/spawner/spawner.cpp
@@ -4,8 +4,8 @@ namespace fnad {
 Spawner::Spawner(sf::Vector2f position, sf::Vector2f size)
     : sf::FloatRect(position, size),
       eng_((std::random_device())()),
-      x_dist_(position.x, position.x + size.x),
-      y_dist_(position.y, position.y + size.y) {}
+      x_dist_(position.x + 5.f, position.x + size.x - 5.f),
+      y_dist_(position.y + 5.f, position.y + size.y - 5.f) {}
 
 float Spawner::getArea() const { return width * height; }
 
