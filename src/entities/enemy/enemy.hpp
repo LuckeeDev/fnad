67b84e011c47e903f0140ac6044da5a7657cbb29
lorenzo <lorenzo.redighieri@studio.unibo.it>
2023-05-26 21:09:49 +0200

    add eng_, time_dist_, time_limit_ members to Enemy

--- src/entities/enemy/enemy.hpp
+++ src/entities/enemy/enemy.hpp
@@ -16,6 +16,12 @@ class Enemy final : public Entity {
 
   sf::Clock clock_;
 
+  std::default_random_engine eng_;
+
+  std::uniform_real_distribution<float> time_dist_;
+
+  sf::Time time_limit_;
+
   void randomMove(sf::Time const&);
 
  public:
